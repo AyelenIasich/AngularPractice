f60707fabcd732710fa855bf3f408be56b85c693
AyelenIasich <ayeleniasich@gmail.com>
2025-01-28 20:05:28 -0300

    Feat: create a new task and delete a task

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-
+import { Task } from '../../models/task.model';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,17 +10,25 @@ import { Component, signal } from '@angular/core';
 })
 export class HomeComponent {
 
-  tasks = signal([
-    "Learn Angular",
-    "Cooking",
-    "Task 3",
+  tasks = signal<Task[]>([
+    { id: crypto.randomUUID(), title: "Learn React", completed: true },
+    { id: crypto.randomUUID(), title: "Learn Angular", completed: false },
+    { id: crypto.randomUUID(), title: "Learn Vue", completed: false },
   ]);
 
   changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
-    const newTasks = input.value;
-    this.tasks.update((tasks) => [...tasks, newTasks]);
+    this.addTask(input.value);
     input.value = "";
+  }
+
+  addTask(title: string) {
+    const newTasks = { id: crypto.randomUUID(), title: title, completed: false };
+    this.tasks.update((tasks) => [...tasks, newTasks]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((_, i) => i !== index));
   }
 
 }
