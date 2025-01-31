980e3531b2046a933c7b861d99062ab603ce1869
AyelenIasich <ayeleniasich@gmail.com>
2025-01-31 20:23:23 -0300

    Computed states

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Task } from '../../models/task.model';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
@@ -17,8 +17,21 @@ export class HomeComponent {
     { id: crypto.randomUUID(), title: "Learn Vue", completed: false },
   ]);
 
-
   newTaskCtrl = new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.pattern(/\S+/)] });
+
+  filter = signal<'all' | 'pending' | 'completed'>('all');
+
+  tasksByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+    if (filter === 'pending') {
+      return tasks.filter(task => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter(task => task.completed);
+    }
+    return tasks;
+  });
 
   changeHandler() {
     if (this.newTaskCtrl.valid) {
@@ -42,12 +55,15 @@ export class HomeComponent {
   }
 
   updateTaskEditingMode(index: number) {
-    this.tasks.update((tasks) => tasks.map((task, i) => i === index ? { ...task, editing: true } : {...task, editing: false}));
+    this.tasks.update((tasks) => tasks.map((task, i) => i === index ? { ...task, editing: true } : { ...task, editing: false }));
   }
 
   updateTaskText(index: number, event: Event) {
     const input = event.target as HTMLInputElement;
+    this.tasks.update((tasks) => tasks.map((task, i) => i === index ? { ...task, title: input.value, editing: false } : task));
+  }
 
-    this.tasks.update((tasks) => tasks.map((task, i) => i === index ? { ...task, title:input.value, editing: false } : task));
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
   }
 }
